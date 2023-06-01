719cf673ca01d037063a8ec9fdb392403133475a
YasinBeraSahin <128741889+YasinBeraSahin@users.noreply.github.com>
2023-06-01 14:34:34 +0300

    dosya ve kütüphane ekleme

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from Insan import Insan
+from Issiz import Issiz         # Diğer dosyaları main dosyasına ekliyoruz ve pandas kütüphanesini ekliyoruz...
+from Calisan import Calisan
+from MaviYaka import MaviYaka
+from BeyazYaka import BeyazYaka
