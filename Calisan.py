0331c8cda8e0425b9ca931344199c912dfa43c36
YasinBeraSahin <128741889+YasinBeraSahin@users.noreply.github.com>
2023-06-01 14:19:08 +0300

    aktarma

---
+++ Calisan.py
@@ -0,0 +1,25 @@
+from Insan import Insan     # Insan dosyasını ekliyoruz...
+class Calisan(Insan):
+    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas):
+        super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk)
+        self.__sektor = sektor
+        self.__tecrube = tecrube
+        self.__maas = maas
+
+    def get_sektor(self):
+        return self.__sektor
+
+    def set_sektor(self, sektor):
+        self.__sektor = sektor
+
+    def get_tecrube(self):
+        return self.__tecrube
+
+    def set_tecrube(self, tecrube):
+        self.__tecrube = tecrube
+
+    def get_maas(self):
+        return self.__maas
+
+    def set_maas(self, maas):
+        self.__maas = maas
--- Issiz.py
+++ Issiz.py
@@ -9,3 +9,24 @@ class Issiz(Insan):
 
     def get_statu(self):
         return self.__statu
+    def statu_bul(self):
+        try:
+            a=self.__maviYaka*0.20
+            b=self.__beyazYaka*0.35       # Hesaplamaları yapıyoruz...
+            c=self.__yonetici*0.45
+            if a>b and a>c:
+                aa="mavi yaka"
+                self.__statu = aa
+            elif b>a and b>c:
+                bb="beyaz yaka"
+                self.__statu=bb
+            else :
+                cc="yönetici"
+                self.__statu=cc
+            return self.__statu
+
+        except ValueError as e:
+            print(e)
+
+    def __str__(self):
+        return f"Ad: {self.get_ad()}\nSoyad: {self.get_soyad()}\nEn Uygun Statü: {self.statu_bul()}"
