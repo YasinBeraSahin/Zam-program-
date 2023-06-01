ec85043791ffef7febdf2aa10159041ae949d160
YasinBeraSahin <128741889+YasinBeraSahin@users.noreply.github.com>
2023-06-01 14:26:38 +0300

    zam hakkı hesaplama
    zamlar hesaplandıktan sonra  __str__ metodu ile return ediyoruz ve en son yeni maaş fonksiyonu oluşturuyoruz.

--- MaviYaka.py
+++ MaviYaka.py
@@ -9,3 +9,23 @@ class MaviYaka(Calisan):
 
     def set_yipranma_payi(self, yipranma_payi):
         self.__yipranma_payi = yipranma_payi
+
+    def zam_hakki(self):
+        try:
+            if self.get_tecrube() < 24:
+                return self.__yipranma_payi * 10
+            elif 24 <= self.get_tecrube() <= 48 and self.get_maas() < 15000:          # Hesaplamaları yapıyoruz...
+                return (self.get_maas() * self.get_tecrube() / (24*200)) + (self.__yipranma_payi * 10)
+            elif self.get_tecrube() > 48 and self.get_maas() < 25000:
+                return (self.get_maas() * self.get_tecrube() / (24*300)) + (self.__yipranma_payi * 10)
+            else:
+                return 0
+        except Exception as e:
+            print(e)
+
+    def __str__(self):
+        return f"Ad: {self.get_ad()}\nSoyad: {self.get_soyad()}\nTecrübe: {self.get_tecrube()} ay\nYeni Maaş: {self.get_maas() + self.zam_hakki()}"
+
+    def yeni_maas(self):           # Yeni maaş fonksiyonu oluşturuyoruz...
+        yeni_maas= self.get_maas()+self.zam_hakki()
+        return yeni_maas
